ff1c96dce56920ff25ebf146f86b2b43b9dc2c5c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-18 23:31:28 +0530

    Power of numbers using recursion

---
+++ Practice_1/Power.py
@@ -0,0 +1,15 @@
+# Power of numbers using recursion
+def powers(a,b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    if b != 0:
+        smallOutput = powers(a, b-1)
+        return a * smallOutput
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+print(powers(a,b))
