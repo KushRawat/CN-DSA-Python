fa3ba588745ac8be3dbb8f0c6006421e13ebdde6
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-21 02:57:30 +0530

    Initial commit

---
+++ Practice_1/sumOfArray.py
@@ -0,0 +1,16 @@
+#program to calculate sum of array using recursion
+
+def sumArray(arr):
+    l = len(arr)
+    if l == 0:
+        return 0
+    smallList = arr[1:]
+    sum1 = sumArray(smallList)
+    sum1 = sum1 + arr[0]
+    return sum1 
+
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+#n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+print(sumArray(arr))
