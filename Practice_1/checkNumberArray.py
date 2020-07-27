b67f26948ac6afbe1de359a4d6fd604210f3a639
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-27 15:38:48 +0530

    final code

--- Practice_1/checkNumberArray.py
+++ Practice_1/checkNumberArray.py
@@ -1,19 +1,21 @@
 def checkNumber(arr, x):
     l = len(arr)
     if l == 0:
-        return True
+        return False
     if x == arr[0]:
         return True
     smallList = arr[1:]
     check = checkNumber(smallList, x)
+    return check
     
 
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)
-n=int(input())
-arr=list(int(i) for i in input().strip().split(' '))
-x=int(input())
+n=int(input()) # size of array
+arr=list(int(i) for i in input().strip().split(' ')) # the array
+x=int(input()) # the number to be found 
+print(arr)
 if checkNumber(arr, x):
     print('true')
 else:
