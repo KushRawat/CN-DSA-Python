305d88b03f1e543af1e3f07af63fab8b236cdd03
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-31 15:41:24 +0530

    initial commit

---
+++ Practice_0/binarySearch.py
@@ -0,0 +1,13 @@
+# Program to carry out binary search using recursion
+def BinarySearch(a, x, si, ei):
+    if si > ei:
+        return -1
+    mid = si + ei // 2
+    if x == a[mid]:
+        return mid
+    elif x > mid:
+        return BinarySearch(a, x, mid + 1, ei)
+    else:
+        return BinarySearch(a, x, si, mid -1)
+
+print(BinarySearch([1, 2, 3, 5, 7, 8], 8, 0, 5))
