0c4e3b70f4eb48178b05ca9a31c5dfecfed97bd3
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-01 16:18:01 +0530

    final code

--- Practice_1/mergeSort.py
+++ Practice_1/mergeSort.py
@@ -1,37 +1,36 @@
-def mergeSort(arr, start, end):
-    size=end-start
-    if size<=1:
-        return
-    mid = (start+end)//2
-    mergeSort(arr, start, mid)
-    mergeSort(arr, mid, end)
-    result = [0] * size
-    i=start
-    j=mid
+def merge(a1,a2,arr):
+    i=0
+    j=0
     k=0
-    while(i<mid and j<end):
-        if(arr[i]<arr[j]):
-            result[k] = arr[i]
-            k += 1
-            i += 1
+    while i<len(a1) and  j<len(a2) :
+        if a1[i]<a2[j]:
+            arr[k]=a1[i]
+            k=k+1
+            i=i+1
         else:
-            result[k] = arr[j]
-            k += 1
-            j += 1
-    while(i<mid):
-        result[k] = arr[i]
-        k += 1
-        i += 1
-    while(j<end):
-        result[k] = arr[j]
-        k += 1
-        j += 1
-    for i in range(0,size):
-        arr[start+i] = result[i]
-   
+            arr[k]=a2[j]
+            k=k+1
+            j=j+1
+    while i<len(a1):
+        arr[k]=a1[i]
+        k=k+1
+        i=i+1
+    while j<len(a2):
+        arr[k]=a2[j]
+        k=k+1
+        j=j+1
+def mergeSort(arr):
+    if len(arr)== 0 or len(arr)==1:
+        return
+    mid=len(arr)//2
+    a1 =arr[0:mid]
+    a2 =arr[mid:]
+    mergeSort(a1)
+    mergeSort(a2)
+    merge(a1,a2,arr)
 
 # Main
 n=int(input())
 arr=list(int(i) for i in input().strip().split(' '))
-mergeSort(arr, 0, n)
+mergeSort(arr)
 print(*arr)
