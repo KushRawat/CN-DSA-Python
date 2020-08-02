e591fa5c7e95e09ad9712562ceab913ea8021787
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-02 23:33:20 +0530

    changes

--- Practice_1/checkPalindrome.py
+++ Practice_1/checkPalindrome.py
@@ -1,13 +1,24 @@
 def checkPalindrome(s, si, ei):
     if si <= ei:
-        return
+        return "true"
     
-    checkPalindrome(s, si + 1, ei - 1)
     while si <= ei:
         if s[si] != s[ei]:
             return "false"
         if s[si] == s[ei]:
             si = si + 1
-            ei = ei - 1
+            ei = ei + 1
+            return 
+    
+    checkPalindrome(s, si + 1, ei - 1)
+    #while si <= ei:
+     #   if s[si] != s[ei]:
+      #      return "false"
+       # if s[si] == s[ei]:
+        #    si = si + 1
+         #   ei = ei + 1
+          #  return 
+
+print(checkPalindrome("abwtwbaa", 0, 3))
         
         
