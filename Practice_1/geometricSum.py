585407ebf114bb743659f0b0efc297b4079efc9c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-03 19:05:06 +0530

    final code

--- Practice_1/geometricSum.py
+++ Practice_1/geometricSum.py
@@ -2,9 +2,9 @@ def sum(n):
     if n == 0:
         return 1
     
-    a = float("%.5f" % (1 / pow(2, n)))
+    a = 1 / pow(2, n)
     
     return sum(n - 1) + a
 
 n = int(input())
-print(sum(n)) 
+print("%.5f" %(sum(n))) 
