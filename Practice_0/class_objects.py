f425afe206a48f5e6a5deb9411a13adb47187baa
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-04 22:56:35 +0530

    objects and classes

---
+++ Practice_0/class_objects.py
@@ -0,0 +1,17 @@
+# Creating classes and objects
+
+class Student: # no parameters or arguments required
+    pass # passing since class is being kept empty for now
+
+s1 = Student() #object
+s2 = Student()
+s3 = Student()
+
+#l1 = listt() # wont work since listt is not defined
+l2 = list()
+
+print(type(s1))
+print(type(s2))
+print(type(s3))
+#print(type(l1))
+print(type(l2))
