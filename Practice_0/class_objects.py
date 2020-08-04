a16c3107aa206249651509df34831d801b9b3cdb
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-05 02:49:22 +0530

    __init__ method multiple arguments

--- Practice_0/class_objects.py
+++ Practice_0/class_objects.py
@@ -1,17 +1,14 @@
 class Student: 
-    def __init__(self): # when any object is created it will pass through __init__ function  
-        self.name = "Kush" # self and created object will have the same address
-        self.rollNumber = "07" # rollNumber is an attribute of self and the created object under 
-                               # this class 
+    def __init__(self, x , y): # added 2 more arguments which will be passed when creating an object
+        self.name = x          # assigned attribute to the arguments
+        self.rollNumber = y  
 
-s1 = Student() # creating an object 
+s1 = Student("Kush", 7)        # passing the arguments which are assigned to the attributes under 
+                                # __init__ function parameter
 print(s1.__dict__)
 
-s2 = Student() # creating a differnet object with a different address
+s2 = Student("Love", 17) 
 print(s2.__dict__)
 
-print(s1,s2) # Both objects have different address but same attributes  
-
-s3 = Student("Love", 17) # this is wrong since now total number of arguments are three: self, "Love", 
-                        #  17. __init__ is passing all these three arguments which is causing an error
+s3 = Student("Mom", 27) 
 print(s3.__dict__)
