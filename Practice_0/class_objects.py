cb67f1bda7e36867455a9095c9d2c1aaf72f80e8
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-05 03:42:03 +0530

    instance methods

--- Practice_0/class_objects.py
+++ Practice_0/class_objects.py
@@ -1,14 +1,13 @@
 class Student: 
-    def __init__(self, x , y): # added 2 more arguments which will be passed when creating an object
-        self.name = x          # assigned attribute to the arguments
-        self.rollNumber = y  
+    def __init__(self, x , y): 
+        self.name = x          
+        self.rollNumber = y 
+    
+    def printStudent(self): # adding functionality to class with argument which is passed 
+                            # automatically as the object
+        print("My name is", self.name, "and my roll number is", self.rollNumber,) 
 
-s1 = Student("Kush", 7)        # passing the arguments which are assigned to the attributes under 
-                                # __init__ function parameter
-print(s1.__dict__)
-
-s2 = Student("Love", 17) 
-print(s2.__dict__)
-
-s3 = Student("Mom", 27) 
-print(s3.__dict__)
+s1 = Student("Kush", 7)    
+s1.printStudent()            #calling function printStudent
+                             # We're calling this method printStudent on object s1 directly     
+Student.printStudent(s1)
