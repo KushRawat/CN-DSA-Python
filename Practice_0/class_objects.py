8aed9730084dec96448483fa2581d29e66e580c7
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-05 00:15:23 +0530

    instance attributes

--- Practice_0/class_objects.py
+++ Practice_0/class_objects.py
@@ -1,17 +1,34 @@
-# Creating classes and objects
+class Student: 
+    pass 
 
-class Student: # no parameters or arguments required
-    pass # passing since class is being kept empty for now
-
-s1 = Student() #object
+s1 = Student() 
 s2 = Student()
 s3 = Student()
 
-#l1 = listt() # wont work since listt is not defined
-l2 = list()
+s1.name = "Kush" # adding attribute name
 
-print(type(s1))
-print(type(s2))
-print(type(s3))
-#print(type(l1))
-print(type(l2))
+s2.rollNumber = "7" # adding attribute rollNumber
+
+s3.name = "Love" # adding attribute name
+s3.rollNumber = "17" # adding attribute rollNumber
+
+print(s1.name) # returns attribute value
+
+print(s1.__dict__) # printing attributes and data of object s1
+print(s2.__dict__) # printing attributes and data of object s2
+print(s3.__dict__) # printing attributes and data of object s3
+
+s1.testkey = "testvalue" # function 1/4 to add attribute and data together to an object 
+print(s1.testkey)
+print(s1.__dict__)
+
+print(hasattr(s1, "name")) #function 2/4 to check whether the object has attribute or not
+
+print(getattr(s1, "name")) # function 3/4 to get attribute data from the object
+print(getattr(s2, "name", "cool")) # function 3/4 returns third argument in case attribute 
+                                   # is not present in the object
+print(getattr(s1, "testkey", "cool")) # function 3/4 does not return 3rd argument if attribute found
+                                      # in object rather returns the attribute found
+print(s1.__dict__)
+delattr(s1, "name") # function 4/4 to delete attribute from the object
+print(s1.__dict__)
