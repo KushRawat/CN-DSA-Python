ff4e14b24da71e7157870898d3fd9f0d608ef508
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-10 22:27:45 +0530

    Protected Members

--- Practice_0/Inheritance.py
+++ Practice_0/Inheritance.py
@@ -2,17 +2,17 @@
 class Vehicle:                          
     def __init__(self, color, maxSpeed): 
         self.color = color               
-        self.__maxSpeed = maxSpeed       
+        self._maxSpeed = maxSpeed       # creating protected variable (_  )   
     
     def getMaxSpeed(self):               
-        return self.__maxSpeed
+        return self._maxSpeed
     
     def setMaxSpeed(self, maxSpeed):     
-        self.__maxSpeed = maxSpeed
+        self._maxSpeed = maxSpeed
 
     def print(self):                      
         print("Color: ", self.color)
-        print("Max Speed: ", self.__maxSpeed)
+        print("Max Speed: ", self._maxSpeed)
 
 class Car(Vehicle):                     
     def __init__(self, color, maxSpeed, numGears, isConvertible): 
@@ -21,10 +21,16 @@ class Car(Vehicle):
         self.isConvertible = isConvertible
 
     def print(self):                        
-        super().print()  # super() here helps rectifying the mistake when we call self.print()
-                         # It calls print function from its parent class
+        print("Color: ", self.color)
+        print("Max Speed: ", self._maxSpeed)  
         print("Number of Gears :", self.numGears)
         print("Is Convertible :", self.isConvertible)
 
-C = Car("Black", 299, 7, False)
-C.print()    
+#C = Car("Black", 299, 7, False)
+#C.print()    
+#print()
+V = Vehicle("Red", 299)
+V.print()
+print()
+V._maxSpeed = 12 # possible but should not be done for protected variables
+V.print()
