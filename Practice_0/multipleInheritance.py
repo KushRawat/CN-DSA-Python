bd5f50e660d353a13a3ef129d82ee37cde309af8
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-11 01:58:46 +0530

    understanding MRO(method resolution order)

--- Practice_0/multipleInheritance.py
+++ Practice_0/multipleInheritance.py
@@ -21,6 +21,8 @@ class Child(Mother, Father):
     def childPrint(self):
         print("The name of child is", self.name)
 
-c = Child()
-c.print()        # Output depends on the order of Inheritence as parameters
-c.childPrint()   # Output depends on the order of Inheritence as parameters
+#c = Child()
+#c.print()        
+#c.childPrint()   
+print(Child.mro())  # This gives the method resolution order, the order in which class and attributes will be 
+                    # accessed respectively
