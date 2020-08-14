3ee36efaffa51a13c4318e234172b271b3cdf0be
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-14 18:19:38 +0530

    implementing abstractmethods

--- Practice_0/abstractClass.py
+++ Practice_0/abstractClass.py
@@ -1,1 +1,43 @@
-from abc import 
+from abc import ABC,abstractmethod     #abc being the module, ABC the class, abstractmethod the decorator
+
+class Automobile(ABC):
+    
+    def __init__(self):
+        print("Automobile created")
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    @abstractmethod
+    def drive(self):
+        pass
+
+class Car(Automobile):
+
+    def __init__(self, name):
+        print("Car created")
+        self.name = name
+
+    def start(self):   # all abstract methods need to be implemented to work 
+        pass
+
+    def stop(self):    # otherwise there will be an error
+        pass
+
+    def drive(self):
+        pass
+
+c1 = Car("G-Wagon")
+
+
+
+
+
+
+
+    
