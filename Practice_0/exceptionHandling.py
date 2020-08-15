8aeefb8b274a69bda30adbe29bb4965882882734
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-15 16:37:57 +0530

    custom class exception

--- Practice_0/exceptionHandling.py
+++ Practice_0/exceptionHandling.py
@@ -1,16 +1,20 @@
  # Learning how to handle errors using try and except
 
+class ZeroDenominatorError(Exception):      # Inheriting Exception which is an in-built python module
+    pass
 while True:
     try:                                      
         num = int(input('Enter number:'))
         den = int(input('Enter number:'))
         if den == 0:
-            raise ZeroDivisionError         # custom exception
-            #raise NameError                # this will cause error here since NameError isnt 
-                                            # called below in exceptions
+            raise ZeroDenominatorError("Zero Denominator error raised")    # raising custom  error     
+            # raise ZeroDenominatorError()      # will work without argument too
         print(num/den)
         break                           
     except (ValueError):                        
         print("Numerator and denominator should be integers")
     except ZeroDivisionError:                
         print("Denominator cannot be zero") 
+    except ZeroDenominatorError:                                            # using exception to handle 
+                                                                            # the custom made error
+        print('Zero as denominator is not defined')
