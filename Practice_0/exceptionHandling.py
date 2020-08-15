4fa7e68f8e7efa04a93e1123cced3031350720de
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-15 17:06:13 +0530

    inheriting error exception exception functionality

--- Practice_0/exceptionHandling.py
+++ Practice_0/exceptionHandling.py
@@ -1,6 +1,7 @@
  # Learning how to handle errors using try and except
 
-class ZeroDenominatorError(Exception):      # Inheriting Exception which is an in-built python module
+class ZeroDenominatorError(ZeroDivisionError):      # Inheriting ZeroDivisionError which makes it 
+                                                    # the parent class here
     pass
 while True:
     try:                                      
@@ -13,8 +14,13 @@ while True:
         break                           
     except (ValueError):                        
         print("Numerator and denominator should be integers")
-    except ZeroDivisionError:                
+    except ZeroDivisionError:                      # this will run since ZeroDivisionError's all 
+                                                # properties has been inherited by our class
         print("Denominator cannot be zero") 
-    except ZeroDenominatorError:                                            # using exception to handle 
-                                                                            # the custom made error
+    except ZeroDenominatorError:                # this will run if it comes before in order                           
         print('Zero as denominator is not defined')
+
+    except:
+        print('Unknown error occured')           # defualt except needs to be in the last since the block runs
+                                             # in order and checks all specified excepts first, if none found 
+                                             # defualt except runs 
