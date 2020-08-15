7974a14e65f131a3e9d7152cbbec53ebfdf235ab
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-15 14:31:47 +0530

    loop

--- Practice_0/exceptionHandling.py
+++ Practice_0/exceptionHandling.py
@@ -1,8 +1,10 @@
  # Learning how to handle errors using try and except
 
-try:                                      # makes the code run normally
-    num = int(input('Enter number:'))
-    den = int(input('Enter number:'))
-    print(num/den)
-except:                        # handles all errors when no error mentioned
-    print("Numerator and denominator should be integers")
+while True:
+    try:                                      
+        num = int(input('Enter number:'))
+        den = int(input('Enter number:'))
+        print(num/den)
+        break                           # correct inputs will break the while loop
+    except:                        
+        print("Numerator and denominator should be integers")
