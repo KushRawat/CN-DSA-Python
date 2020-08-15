f1c9c4096614308d5c342cba6789b9b6601e6d88
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-15 14:27:11 +0530

    no error mentioned

--- Practice_0/exceptionHandling.py
+++ Practice_0/exceptionHandling.py
@@ -4,5 +4,5 @@ try:                                      # makes the code run normally
     num = int(input('Enter number:'))
     den = int(input('Enter number:'))
     print(num/den)
-except ValueError:                        # handles when specified error comes into picture
+except:                        # handles when specified error comes into picture
     print("Numerator and denominator should be integers")
