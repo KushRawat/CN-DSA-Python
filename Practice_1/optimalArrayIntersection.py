a4a12c0bf7012fd54a401a127d2e55424142969c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-20 16:51:21 +0530

    final code

--- Practice_1/optimalArrayIntersection.py
+++ Practice_1/optimalArrayIntersection.py
@@ -4,7 +4,7 @@ def mergeIntersection(arr1, arr2):  # will be used in intersection function
     i = 0
     j = 0
     
-    while i < len(arr1) or j < len(arr2):
+    while i < len(arr1) and j < len(arr2):
         if arr1[i] < arr2[j]:
             i = i + 1
         elif arr1[i] == arr2[j]:
