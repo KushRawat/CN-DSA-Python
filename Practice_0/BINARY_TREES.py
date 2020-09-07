0beacd47a7452f132f15f103e5af7f4fc7deef72
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-07 23:54:33 +0530

    initial commit

---
+++ Practice_0/BINARY_TREES.py
@@ -0,0 +1,8 @@
+# Learning how to create Binary Trees
+
+class BinaryTreeNode:
+
+    def __init__(self, data):    # constructor
+        self.data = data        # taking data as input 
+        self.left = None        # initially child has nothing 
+        self.right = None       # "                          "
