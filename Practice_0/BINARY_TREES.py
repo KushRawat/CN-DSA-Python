2606df8f3ab190aaf0be4d1b216779e0ac3dec16
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-08 19:08:52 +0530

    creating and printing binary tree

--- Practice_0/BINARY_TREES.py
+++ Practice_0/BINARY_TREES.py
@@ -12,15 +12,28 @@ def printTree(root):            # creating function to print tree using recursio
     if root == None:            # base case
         return
 
-    print(root.data)            # small calculation
+    print(root.data, end = ':' )            # small calculation
+    
+    if root.left != None:                       # to avoid error since root.left wil be None in the end
+        print("L", root.left.data, end = ',')
+    
+    if root.right != None:
+        print("R", root.right.data, end = '')
+    print()
+    
     printTree(root.left)        # recursion calls
     printTree(root.right)
 
 btn1 = BinaryTreeNode(1)        # creating Nodes with data
-btn2 = BinaryTreeNode(4)        
-btn3 = BinaryTreeNode(7)
+btn2 = BinaryTreeNode(2)        
+btn3 = BinaryTreeNode(3)
+btn4 = BinaryTreeNode(4)
+btn5 = BinaryTreeNode(5)
+
 
 btn1.left = btn2                # linking the nodes in a tree structure
 btn1.right = btn3
+btn2.left = btn4
+btn2.right = btn5
 
 printTree(btn1)
