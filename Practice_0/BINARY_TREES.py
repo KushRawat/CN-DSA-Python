fa5b3a30f613adaf9d51cdda84d8173463bef6b6
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-13 20:14:29 +0530

    print Nodes at depth k - another method

--- Practice_0/BINARY_TREES.py
+++ Practice_0/BINARY_TREES.py
@@ -78,15 +78,26 @@ def printDepthK(root, k):  # to find depth at K level, first level being 0
     if root is None:
         return 0
 
-    if k is 0:
+    if k == 0:
         print(root.data, end = " ")
         return
 
     printDepthK(root.left, k - 1)
     printDepthK(root.right, k - 1)
 
+def printDepthKV2(root, k, d = 0):
 
+    if root == None:
+        return 
+
+    if k == d:
+        print(root.data, end = ' ')
+        return
+
+    printDepthKV2(root.left, k , d + 1)
+    printDepthKV2(root.right, k , d + 1)
 
 root = treeInput()
 
 printDepthK(root, 2)
+printDepthKV2(root, 2)
