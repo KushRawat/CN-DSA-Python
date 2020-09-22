bdad4042c56f402e303874c9ebe57c8f98a230c8
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-22 13:55:17 +0530

    connecting nodes

--- Practice_0/GENERIC_TREE.py
+++ Practice_0/GENERIC_TREE.py
@@ -7,5 +7,19 @@ class GTnode:
         self.data = data                # data to be pased
         self.children = list()          # an empty list which will be having children 
 
-root = GTnode(5)
-print(root)
+n1 = GTnode(5)
+n2 = GTnode(2)
+n3 = GTnode(9)
+n4 = GTnode(8)
+n5 = GTnode(7)
+n6 = GTnode(15)
+n7 = GTnode(1)
+
+n1.children.append(2)
+n1.children.append(9)
+n1.children.append(8)
+n1.children.append(7)
+
+n3.children.append(15)
+n3.children.append(1)
+
