484d9ef588c2c9f4b54f7b097f2c29448aab2772
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-24 16:49:18 +0530

    BT2 is binary tree balanced

--- Practice_0/BINARY_TREES.py
+++ Practice_0/BINARY_TREES.py
@@ -110,7 +110,32 @@ def removeLeaves(root):    # BT - 2
     
     return root                    # returning root with updated tree
 
+def height(root):      # for checking isbalanced function
+
+    if root is None:
+        return 0
+
+    return 1 + max(height(root.left), height(root.right))
+
+def  isBalanced(root):      # BT - 2
+
+    if root is None:        # if tree is empty, it is balanced
+        return True
+
+    lh = height(root.left)
+    rh = height(root.right)
+
+    if lh - rh > 1 or rh - lh > 1:
+        return False
+
+    isLeftBalanced = isBalanced(root.left)
+    isRightBalanced = isBalanced(root.right)
+
+    if isLeftBalanced and isRightBalanced:
+        return True
+    else:
+        return False
+
 root = treeInput()
 printTree(root)
-root = removeLeaves(root)        # returning root with updated tree in root variable
-printTree(root)
+print(isBalanced(root))
