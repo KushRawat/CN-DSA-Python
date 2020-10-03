31655381c00ed6059c0c3d56cb115ca490363af5
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-03 23:52:04 +0530

    final code

--- Practice_1/t1splitArrayNew.py
+++ Practice_1/t1splitArrayNew.py
@@ -2,8 +2,27 @@ def split(arr, i, sum):
     
     if i == len(arr):
         
-         if sum == 0:
+        if sum == 0:
             return True
         
         else:
             return False
+
+    if ((arr[i] % 5) == 0):
+        return split(arr, i + 1, sum + arr[i])
+    
+    elif ((arr[i] % 3) == 0):
+        return split(arr, i + 1, sum - arr[i])
+
+    else:
+        ans1 = split(arr, i + 1, sum + arr[i])
+        ans2 = split(arr, i + 1, sum - arr[i])
+        return ans1 or ans2
+
+n = int(input())
+arr = [int(ele) for ele in input().split()]
+ans = split(arr, 0, 0)
+if ans is True:
+    print('true')
+else:
+    print('false')
