c20fc5427239b39fc4ef9d246d6a759e37f5614b
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-11 18:29:04 +0530

    code correction

--- Practice_1/LLt_mergeLLs.py
+++ Practice_1/LLt_mergeLLs.py
@@ -9,27 +9,30 @@ class Node :
 
 
 def mergeTwoSortedLinkedLists(head1,head2):
-    
-    FH = head1
-    FT = head1
-    
-    while head1 is not None and head2 is not None:
-        
-        if head1.data < head2.data:
-            
-            FT.next = head2
-            FT = FT.next
-            head1 = head1.next
-            head2 = head2.next
-            
+    if head1==None:
+        return head2
+    if head2==None:
+        return head1
+    if head1.data<=head2.data:
+        head=head1
+        head1=head1.next
+    else:
+        head=head2
+        head2=head2.next
+    curr=head
+    while head1 and head2:
+        if head1.data<=head2.data:
+            curr.next=head1
+            head1=head1.next
         else:
-            
-            FT.next = head2
-            
-            head1 = head1.next
-            head2 = head2.next
-            
-            
+            curr.next=head2
+            head2=head2.next
+        curr=curr.next
+    if head1==None:
+        curr.next=head2
+    else:
+        curr.next=head1
+    return head
     
 #Taking Input Using Fast I/O
 def takeInput() :
@@ -67,8 +70,10 @@ def printLinkedList(head) :
 
 
 #main
-head1 = takeInput()
-head2 = takeInput()
+n=int(input())
+for i in range(n):
+    head1 = takeInput()
+    head2 = takeInput()
 
-newHead = mergeTwoSortedLinkedLists(head1, head2)
-printLinkedList(newHead)
+    newHead = mergeTwoSortedLinkedLists(head1, head2)
+    printLinkedList(newHead)
