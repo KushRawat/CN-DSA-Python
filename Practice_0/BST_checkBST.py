49ca48a6e78158ea1f32f385080bc7d757df1746
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-11-15 05:16:36 +0530

    another method to check for BST

--- Practice_0/BST_checkBST.py
+++ Practice_0/BST_checkBST.py
@@ -54,3 +54,15 @@ def isBST2(root):
 
     return minimum, maximum, isTreeBST
 
+def isBST3(root, min_range, max_range):
+
+    if root is None:
+        return True
+
+    if root.data < min_range or root.data > max_range:
+        return False
+
+    isLeftWithinConstraints = isBST3(root.left, min_range, root.data - 1)
+    isRightWithinConstraints = isBST3(root.right, root.data, max_range)
+
+    return isLeftWithinConstraints and isRightWithinConstraints
